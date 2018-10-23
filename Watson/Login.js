73db9cd0763abfd43dd9eb56d4fc314fe8e5cb5f
KikeOficial <kikesantana52@gmail.com>
2018-10-23 00:04:58 -0500

    Modificaciones de watson

--- Watson/Login.js
+++ Watson/Login.js
@@ -1,63 +1,34 @@
-  // export function sendQuery(request_text) {
   let contexto;
-  var AssistantV1 = require('watson-developer-cloud/assistant/v1');
-
-  var watsonAssistant = new AssistantV1({
+  let AssistantV1 = require('watson-developer-cloud/assistant/v1');
+  let watsonAssistant = new AssistantV1({
       version: '2018-09-20',
       username: '7ad9192b-3151-4053-9dab-420ee91726fa',
       password: 'BqVn458Ttarp',
       url: 'https://gateway.watsonplatform.net/assistant/api'
     });
-  var params = {
+  let params = {
     workspace_id: 'aaf31d3d-9705-44f8-8a7e-340571891d3c',
     options:true,
     input: {'text': "Quiero saber mis contraseñas"},
   }
 
-  let promiseResponse = new Promise((resolve, reject)=>{
-    watsonAssistant.message(params,  function(err, response) {
-      if (err)
+  watsonAssistant.message(params,  function(err, response) {
+    // Si hay un error
+      if (err){
         console.log('error:', err);
+      }
+      // Si tiene opciones imprimir las opciones
+      else if (response.output.generic[0].options) {
+        response.output.generic[0].options.forEach(function(element) {
+          // TODO: Por cada opcion imprimir un boton
+        });
+      // Si es una imagen imprimir la imagen
+      }else if (response.output.generic[]) {
+
+      }
+      // Si mo es ninguna de las anteriores solo mandar respuesta
       else{
-        if (response.output.generic[0].options) {
-          // response.output.generic[0].options.forEach(function(element) {
-          //   console.log(element);
-          // });
-        // params.context = {'context': response.context};
-        console.log(response);
-        resolve(response.context);
-          // console.log(response.output.generic[0].title);
-        }else{
-          console.log(response.output.text[0]);
+          // TODO: Imprimir respuesta
         }
       }
-
     });
-
-  })
-  promiseResponse.then((response)=>{
-    console.log("------------------------------------");
-    params.input = {'text': "RTC"};
-    params.context = response;
-    console.log(params);
-    watsonAssistant.message(params,  function(err, response) {
-      if (err)
-        console.log('error:', err);
-      else{
-        if (response.output.generic[0].options) {
-          // response.output.generic[0].options.forEach(function(element) {
-          //   console.log(element);
-          // });
-        contexto = (response);
-          // console.log(response.output.generic[0].title);
-        }else{
-          console.log(response);
-        }
-      }
-
-    });
-  })
-
-
-
-// }
